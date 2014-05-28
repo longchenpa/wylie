9ea7b30979c604a99384d898ebe59b1d44c61623
Maxim Sokhatsky <maxim@synrc.com>
2014-05-28 19:02:09 +0300

    first commit

---
+++ wylie.erl
@@ -0,0 +1,166 @@
+-module(wylie).
+-author("རྣམ་དག་སྟོན་པ").
+-copyright("Longchen Nyingthig").
+-compile(export_all).
+
+% Extended Wylie Tibetan Script EWTS UTF-8 Converter
+
+constants() -> [
+{"kh",{16#0f41,16#0f91}},
+{"k",{16#0f40,16#0f90}},
+{"g",{16#0f42,16#0f92}},
+{"gh",{[16#0f42,16#0fb7],[16#0f92,16#0fb7]}},
+{"g+h",{[16#0f42,16#0fb7],[16#0f92,16#0fb7]}},
+{"ng",{16#0f44,16#0f94}},
+{"c",{16#0f45,16#0f95}},
+{"ch",{16#0f46,16#0f96}},
+{"j",{16#0f47,16#0f97}},
+{"ny",{16#0f49,16#0f99}},
+{"T",{16#0f4a,16#0f9a}},
+{"-t",{16#0f4a,16#0f9a}},
+{"Th",{16#0f4b,16#0f9b}},
+{"-th",{16#0f4b,16#0f9b}},
+{"D",{16#0f4c,16#0f9c}},
+{"-d",{16#0f4c,16#0f9c}},
+{"Dh",{[16#0f4c,16#0fb7],[16#0f9c,16#0fb7]}},
+{"D+h",{[16#0f4c,16#0fb7],[16#0f9c,16#0fb7]}},
+{"-dh",{[16#0f4c,16#0fb7],[16#0f9c,16#0fb7]}},
+{"-d+h",{[16#0f4c,16#0fb7],[16#0f9c,16#0fb7]}},
+{"N",{16#0f4e,16#0f9e}},
+{"-n",{16#0f4e,16#0f9e}},
+{"t",{16#0f4f,16#0f9f}},
+{"th",{16#0f50,16#0fa0}},
+{"d",{16#0f51,16#0fa1}},
+{"dh",{[16#0f51,16#0fb7],[16#0fa1,16#0fb7]}},
+{"d+h",{[16#0f51,16#0fb7],[16#0fa1,16#0fb7]}},
+{"n",{16#0f53,16#0fa3}},
+{"p",{16#0f54,16#0fa4}},
+{"ph",{16#0f55,16#0fa5}},
+{"b",{16#0f56,16#0fa6}},
+{"bh",{[16#0f56,16#0fb7],[16#0fa6,16#0fb7]}},
+{"b+h",{[16#0f56,16#0fb7],[16#0fa6,16#0fb7]}},
+{"m",{16#0f58,16#0fa8}},
+{"ts",{16#0f59,16#0fa9}},
+{"tsh",{16#0f5a,16#0faa}},
+{"dz",{16#0f5b,16#0fab}},
+{"dzh",{[16#0f5b,16#0fb7],[16#0fab,16#0fb7]}},
+{"dz+h",{[16#0f5b,16#0fb7],[16#0fab,16#0fb7]}},
+{"w",{16#0f5d,16#0fad}},
+{"zh",{16#0f5e,16#0fae}},
+{"z",{16#0f5f,16#0faf}},
+{"'",{16#0f60,16#0fb0}},
+{[16#2018],{16#0f60,16#0fb0}}, % typographic quotes
+{[16#2019],{16#0f60,16#0fb0}},
+{"y",{16#0f61,16#0fb1}},
+{"r",{16#0f62,16#0fb2}},
+{"l",{16#0f63,16#0fb3}},
+{"sh",{16#0f64,16#0fb4}},
+{"Sh",{16#0f65,16#0fb5}},
+{"-sh",{16#0f65,16#0fb6}},
+{"s",{16#0f66,16#0fb7}},
+{"h",{16#0f67,16#0fb8}},
+{"W",{16#0f5d,16#0fba}},
+{"Y",{16#0f61,16#0fbb}},
+{"R",{16#0f6a,16#0fbc}},
+{"f",{[16#0f55,16#0f39],unknown}},
+{"v",{[16#0f56,16#0f39],unknown}}].
+
+vowels() -> [
+{"a",16#0F68}, % a-chen
+{"A",16#0f71},
+{"i",16#0f72},
+{"I",[16#0f71,16#0f72]},
+{"u",16#0f74},
+{"U",[16#0f71,16#0f74]},
+{"e",16#0f7a},
+{"ai",16#0f7b},
+{"o",16#0f7c},
+{"au",16#0f7d},
+{"-i",16#0f80},
+{"-I",[16#0f71,16#0f80]}].
+
+sanskrit_vowels() -> [
+{"r-i",[16#0fb2,16#0f80]},
+{"r-I",[16#0fb2,16#0f71,16#0f80]},
+{"l-i",[16#0fb3,16#0f80]},
+{"l-I",[16#0fb3,16#0f71,16#0f80]}].
+
+complex_vowels() -> [
+{"r-i",{[16#0f62,16#0f80],[16#0fb2,16#0f80]}},
+{"r-I",{[16#0f62,16#0f71,16#0f80],[16#0fb2,16#0f71,16#0f80]}}, 
+{"l-i",{[16#0f63,16#0f80],[16#0fb3,16#0f80]}},
+{"l-I",{[16#0f63,16#0f71,16#0f80],[16#0fb3,16#0f71,16#0f80]}}].
+
+final() -> [
+{"M",{1,16#0f7e,"M"}}, % anusvara / bindu / circle above / nga ro
+{"H",{1,16#0f7f,"H"}}, % visarga / rnam bcad
+{"~M`",{1,16#0f82,"M"}}, % crescent, bindu & nada
+{"~M",{1,16#0f83,"M"}}, % crescent & bindu
+{"?",{1,16#0f84,"?"}}, % halanta / srog med
+{"X",{1,16#0f37,"X"}}, % small circle under
+{"~X",{1,16#0f35,"X"}}, % small circle w/ crescent under
+{"^",{1,16#0f39,"^"}}]. % tsa-phru
+
+other() -> [
+{"0",16#0f20},
+{"1",16#0f21},
+{"2",16#0f22},
+{"3",16#0f23},
+{"4",16#0f24},
+{"5",16#0f25},
+{"6",16#0f26},
+{"7",16#0f27},
+{"8",16#0f28},
+{"9",16#0f29},
+{" ",16#0f0b},
+{"*",16#0f0c},
+{"/",16#0f0d},
+{"//",16#0f0e},
+{";",16#0f0f},
+{"|",16#0f11},
+{"!",16#0f08},
+{":",16#0f14},
+{"_"," "},
+{"=",16#0f34},
+{"<",16#0f3a},
+{">",16#0f3b},
+{"(",16#0f3c},
+{")",16#0f3d},
+{"@",16#0f04},
+{"#",16#0f05},
+{"$",16#0f06},
+{"%",16#0f07}].
+
+transcode({wylie,Text}) ->
+    Table = lists:flatten([
+        constants(),vowels(),sanskrit_vowels(),complex_vowels(),final(),other()]),
+    Res = lists:reverse(t(4,Text,Table,[],0)),
+%    io:format("~ts~n",[Res]),
+    lists:flatten(Res).
+
+t(0,S,_,L,_) -> L;
+t(_,[],_,L,_) -> L;
+t(N,String,Dictionary,Letters,P) when P > 2 -> t(N,String,Dictionary,Letters,0); % clear stack
+t(N,String,Dictionary,Letters,P) ->
+    R = lists:keyfind(lists:sublist(String,N),1,Dictionary),
+    case R of
+        {Key,Value} ->
+            {Letter,Stack} = case Value of
+                Value when Value == 16#0f0b -> {Value,5};
+                {A,B} -> case P of
+                    0 -> {[A],1};
+                    1 -> {[B],5}; % TODO: stack more than one
+                    2 -> {[B],5};
+                    3 -> {[B],5} end;
+                Value when is_list(Value) -> {Value,P+1};
+                Value when is_integer(Value) -> case Key of "a" -> {[],5}; _ -> {[Value],5} end
+            end,
+            case length(String) > N of
+                false -> [Letter|Letters];
+                true -> t(4,lists:sublist(String,N+1,length(String)),
+                            Dictionary,[Letter|Letters],Stack) end;
+        false -> t(N-1,String,Dictionary,Letters,P) end.
+
+tibetan(Wylie) ->
+    transcode({wylie,Wylie}).
+
